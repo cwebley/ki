6c224fafe1d7c534091ee16bd90d6feb9fa6605e
cwebley <cameron.webley@gmail.com>
2015-05-26 00:10:01 -0500

    react router using the new context stuff, fixed double render and other problems

--- web/src/js/components/app.js
+++ web/src/js/components/app.js
@@ -1,21 +1,14 @@
 var React = require('react'),
-    AboutPage = require('../pages/about'),
-    TourneyPage = require('../pages/tournaments'),
-    StatsPage = require('../pages/stats'),
-    FaqPage = require('../pages/faq'),
     Router = require('react-router'),
-    DefaultRoute = Router.DefaultRoute,
-    Link = Router.Link,
-    Route = Router.Route,
-    RouteHandler = Router.RouteHandler;
+    RouteHandler = Router.RouteHandler,
+    Link = Router.Link;
 
 var App =
   React.createClass({
-    // getInitialState:function(){
-    //     return {
-    //         loggedIn: this.props.loggedIn
-    //     };
-    // },
+    contextTypes: {
+      router: React.PropTypes.func.isRequired
+    },
+
     render:function(){
         return (
             <div>
@@ -26,7 +19,7 @@ var App =
                         <li><Link to="stats">Stats</Link></li>
                         <li><Link to="faq">FAQ</Link></li>
                         <li>
-                            {this.state.loggedIn ? (<Link to="logout">Log Out</Link>) : (<Link to="login">Sign In</Link>)}
+                           WELCOME KID
                         </li>
                     </ul>
 
@@ -36,20 +29,6 @@ var App =
             </div>
         );
     }
-});
-
-var routes = (
-    <Route name="app" path="/" handler={App}>
-        <Route name="about" handler={AboutPage}/>
-        <Route name="stats" handler={StatsPage}/>
-        <Route name="tournaments" handler={TourneyPage}/>
-        <Route name="faq" handler={FaqPage}/>
-        <DefaultRoute handler={AboutPage}/>
-    </Route>
-);
-
-Router.run(routes, function (Handler) {
-  React.render(<Handler/>, document.body)
 });
 
 module.exports = App;
--- web/src/js/main.js
+++ web/src/js/main.js
@@ -1,14 +1,29 @@
 var App = require('./components/app'),
 	React = require('react'),
 	a11y = require('react-a11y'), // browser console helper. can be removed later.
-	api = require('./utils/api');
+	api = require('./utils/api'),
+    Router = require('react-router'),
+    Route = Router.Route,
+    DefaultRoute = Router.DefaultRoute,
+    AboutPage = require('./pages/about'),
+    TourneyPage = require('./pages/tournaments'),
+    StatsPage = require('./pages/stats'),
+    FaqPage = require('./pages/faq');
 
 //TODO: req param when router used.
 // var tourneyName = 'ReturningChamp-Obvi' 
 api.getTournamentIndex() // load tournament data for first time into store.
 
-
-React.render(
-	<App />,
-	document.getElementById('main')
+var routes = (
+    <Route name="app" path="/" handler={App}>
+        <Route name="about" handler={AboutPage}/>
+        <Route name="stats" handler={StatsPage}/>
+        <Route name="tournaments" handler={TourneyPage}/>
+        <Route name="faq" handler={FaqPage}/>
+        <DefaultRoute handler={AboutPage}/>
+    </Route>
 );
+
+Router.run(routes, function (Handler) {
+  React.render(<Handler/>, document.getElementById('root'))
+});
