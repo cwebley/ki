e8b7be4b87737f98a9eaacbc818138bd60c8bdc8
cwebley <cameron.webley@gmail.com>
2015-09-10 01:51:40 -0500

    remove rash from character list and dont init characters from season 3

--- modules/constants/index.js
+++ modules/constants/index.js
@@ -1,6 +1,6 @@
 var ConstantsInterface = {};
 
-ConstantsInterface.characters = ['jago','wulf','thunder','sadira','orchid','glacius','spinal','fulgore','tj','maya','kanra','riptor','omen','aganos','hisako','cinder','aria','rash']
+ConstantsInterface.characters = ['jago','wulf','thunder','sadira','orchid','glacius','spinal','fulgore','tj','maya','kanra','riptor','omen','aganos','hisako','cinder','aria']
 
 ConstantsInterface._STARTING_PWR_STOCK = 4;
 ConstantsInterface._INSPECT_COUNT = 9;
