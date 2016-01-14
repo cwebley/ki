1d1593f0d4cccf983d6e3b3c4f7fe276851af3b7
cwebley <cameron.webley@gmail.com>
2016-01-13 21:35:46 -0600

    query export change

--- api/persistence/pg/index.js
+++ api/persistence/pg/index.js
@@ -2,7 +2,7 @@ import { defaultConfigs } from './config';
 import { TS_FORMAT, now, pgSetup } require('./pg');
 
 function query(sql, params, cb) {
-	pgSetup(defaultConfigs.pg, sql, params, cb);
+	return pgSetup(defaultConfigs.pg);
 }
 
 export { TS_FORMAT, now, query };
