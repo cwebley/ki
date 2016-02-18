12294c3d19413c18a4300fd3b2b2d8df75d42e2e
cwebley <cameron.webley@gmail.com>
2016-02-18 02:38:44 -0600

    fix test config

--- api/config/config.test.js
+++ api/config/config.test.js
@@ -1,18 +1,4 @@
-import config from './config/global.js';
-
-var config = {
-    "env": 'test',
-    "pg": {
-		"database": "test",
-		"username": "ki",
-		"password": "123456789FOOBARBAZ",
-		"server": "localhost"
-	},
-	"redis": {
-		"host": "127.0.0.1",
-		"port": 6380
-	},
-};
+import config from './config.global.js';
 
 config.env = "test";
 config.pg.database = "test";
