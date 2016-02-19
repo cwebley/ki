1032d98511e388f5d194c07a50551989b91275a4
cwebley <cameron.webley@gmail.com>
2016-02-19 01:46:57 -0600

    loggerr running on app.js

--- api/app.js
+++ api/app.js
@@ -1,51 +1,17 @@
 import { query } from './persistence/pg';
 import redis from './persistence/redis';
-var Loggerr = require('loggerr');
+import Loggerr from 'loggerr';
+import cliFormatter from 'loggerr/formatters/cli';
 
 var log = new Loggerr({
-    level: Loggerr.INFO
+    level: Loggerr.INFO,
+    formatter: cliFormatter
 });
-// log.error(new Error('My error message'));
+log.error(new Error('My error message'));
 
-log.info('Something happened', {
+log.debug('Something happened', {
     foo: 'info about what happened'
 });
-
-// import chalk from 'chalk';
-//
-// var Logtastic = require('logtastic/Logger');
-//
-// console.log("CRED: ", chalk.red)
-// function formatter (date, level, data) {
-//     var color;
-//     switch (level) {
-//         case Logtastic.EMERGENCY:
-//         case Logtastic.ALERT:
-//         case Logtastic.CRITICAL:
-//         case Logtastic.ERROR:
-//             color = chalk.red;
-//             break;
-//         case Logtastic.WARNING:
-//         case Logtastic.NOTICE:
-//             color = chalk.yellow;
-//             break;
-//         case Logtastic.INFO:
-//         case Logtastic.DEBUG:
-//             color = chalk.white;
-//             break;
-//     }
-//     console.log("COLOR: ", color)
-//     return color(data.msg);
-// };
-//
-// var log = new Logtastic({
-//     formatter: formatter
-// });
-//
-// log.outfile = 'stdout.log';
-// log.level= log.INFO;
-//
-// log.error(new Error('My error message'));
 
 const sql = 'INSERT INTO users (name, password) VALUES ($1, $2)';
 const params = ['Cameron', 'asdfasdf'];
