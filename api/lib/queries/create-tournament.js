f7db549f7e9ac5cadc8e3aa993a7a2afa9e2a402
cwebley <cameron.webley@gmail.com>
2016-02-28 12:33:53 -0600

    code formatting for create touranment transaction

--- api/lib/queries/create-tournament.js
+++ api/lib/queries/create-tournament.js
@@ -5,72 +5,20 @@ export default function createTournamentQuery (db, opts, cb) {
 	// begin transaction
 	db.query('BEGIN', () => {
 
-		const tournamentsSql = `
-			INSERT INTO tournaments
-				(uuid, name, slug, goal)
-			VALUES
-				($1, $2, $3, $4)
-		`;
-		const tournamentsParams = [opts.uuid, opts.name, opts.slug, opts.goal];
-
-		db.query(tournamentsSql, tournamentsParams, (err, results) => {
+		addToTournamentsTable(db, opts, (err, results) => {
 			if (err) {
-				log.error(err, {
-					sql: tournamentsSql,
-					params: tournamentsParams
-				});
 				return rollback(db, err, cb);
 			}
-
-			const tournamentUsersSql = `
-				INSERT INTO tournamentUsers
-					(tournamentUuid, userUuid)
-				VALUES
-					($1, $2),
-					($3, $4)
-			`;
-			const tournamentUsersParams = [opts.uuid, opts.user.uuid, opts.uuid, opts.opponent.uuid];
-
-			db.query(tournamentUsersSql, tournamentUsersParams, (err, results) => {
+			addToTournamentUsersTable(db, opts, (err, results) => {
 				if (err) {
-					log.error(err, {
-						sql: tournamentUsersSql,
-						params: tournamentUsersParams
-					});
 					return rollback(db, err, cb);
 				}
-
-				// this query needs to be constructed based on the characters passed in
-				let tournamentCharactersSql = `
-					INSERT INTO tournamentCharacters
-						(tournamentUuid, userUuid, characterUuid)
-					VALUES
-				`;
-				let tournamentCharactersParams = [];
-
-				opts.user.characters.forEach((c, i) => {
-					tournamentCharactersSql += `($${3*i+1}, $${3*i+2}, $${3*i+3}),\n`;
-					tournamentCharactersParams.push(opts.uuid, opts.user.uuid, c.uuid);
-				});
-				const userCharLength = tournamentCharactersParams.length
-				opts.opponent.characters.forEach((c, i) => {
-					tournamentCharactersSql += `($${3 * i + 1 + userCharLength}, $${3 * i + 2 + userCharLength}, $${3 * i + 3 + userCharLength}),\n`;
-					tournamentCharactersParams.push(opts.uuid, opts.opponent.uuid, c.uuid);
-				});
-
-				// trim off the extra new line and extra comma at the end
-				tournamentCharactersSql = tournamentCharactersSql.slice(0, -2);
-
-				db.query(tournamentCharactersSql, tournamentCharactersParams, (err, results) => {
-					console.log("CHARS RES: ", err, results)
-					log.error(err, {
-						sql: tournamentCharactersSql,
-						params: tournamentCharactersParams
-					});
+				addToTournamentCharactersTable(db, opts, (err, results) => {
 					if (err) {
 						return rollback(db, err, cb);
 					}
 
+					// end transaction
 					db.query('COMMIT', () => {
 						// return the tournament object
 						return cb(null, {
@@ -83,6 +31,83 @@ export default function createTournamentQuery (db, opts, cb) {
 				});
 			});
 		});
+	});
+}
+
+function addToTournamentsTable (db, opts, cb) {
+	const sql = `
+		INSERT INTO tournaments
+			(uuid, name, slug, goal)
+		VALUES
+			($1, $2, $3, $4)
+	`;
+	const params = [opts.uuid, opts.name, opts.slug, opts.goal];
+
+	db.query(sql, params, (err, results) => {
+		if (err) {
+			log.error(err, {
+				sql: sql,
+				params: params
+			});
+		}
+		return cb(err, results);
+	});
+}
+
+function addToTournamentUsersTable (db, opts, cb) {
+	const tournamentUsersSql = `
+		INSERT INTO tournamentUsers
+			(tournamentUuid, userUuid)
+		VALUES
+			($1, $2),
+			($3, $4)
+	`;
+	const tournamentUsersParams = [opts.uuid, opts.user.uuid, opts.uuid, opts.opponent.uuid];
+
+	db.query(tournamentUsersSql, tournamentUsersParams, (err, results) => {
+		if (err) {
+			log.error(err, {
+				sql: tournamentUsersSql,
+				params: tournamentUsersParams
+			});
+		}
+		cb(err, results);
+	});
+}
+
+function addToTournamentCharactersTable (db, opts, cb) {
+	// this query needs to be constructed based on the characters passed in
+	let tournamentCharactersSql = `
+		INSERT INTO tournamentCharacters
+			(tournamentUuid, userUuid, characterUuid)
+		VALUES
+	`;
+	let tournamentCharactersParams = [];
+
+	opts.user.characters.forEach((c, i) => {
+		tournamentCharactersSql += `($${3*i+1}, $${3*i+2}, $${3*i+3}),\n`;
+		tournamentCharactersParams.push(opts.uuid, opts.user.uuid, c.uuid);
+	});
+
+	const userCharLength = tournamentCharactersParams.length;
+
+	opts.opponent.characters.forEach((c, i) => {
+		tournamentCharactersSql += `($${3 * i + 1 + userCharLength}, $${3 * i + 2 + userCharLength}, $${3 * i + 3 + userCharLength}),\n`;
+		tournamentCharactersParams.push(opts.uuid, opts.opponent.uuid, c.uuid);
+	});
+
+	// trim off the extra new line and extra comma at the end
+	tournamentCharactersSql = tournamentCharactersSql.slice(0, -2);
+
+	db.query(tournamentCharactersSql, tournamentCharactersParams, (err, results) => {
+		// log the error but handle it in the calling func
+		if (err) {
+			log.error(err, {
+				sql: tournamentCharactersSql,
+				params: tournamentCharactersParams
+			});
+		}
+		cb(err, results);
 	});
 }
 
