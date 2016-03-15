9efb09d1c8402b76be74f1cb911c0c1d2bdaa19a
cwebley <cameron.webley@gmail.com>
2016-03-14 22:50:38 -0500

    webpack on front end with react react-router babel etc

---
+++ app/components/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = React.createClass({
+	render: function () {
+		return (
+			<div className="home-page">
+				<h1>The app has react router</h1>
+				<p>Here\'s a paragraph</p>
+			</div>
+		);
+	}
+});
+
+export default Home;
--- clear.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM games;
-DELETE FROM tournamentUsers;
-DELETE FROM tournaments;
-DELETE FROM charactersData;
-DELETE FROM seeds;
