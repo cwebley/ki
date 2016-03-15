b154b889ab6b00e7f525349d57f9d71ac13c9363
cwebley <cameron.webley@gmail.com>
2016-03-15 01:49:51 -0500

    some random material ui stuff as a test

--- app/components/main-layout.js
+++ app/components/main-layout.js
@@ -1,21 +1,76 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+import getMuiTheme from 'material-ui/lib/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/lib/MuiThemeProvider';
+import { deepOrange500 } from 'material-ui/lib/styles/colors';
+import RaisedButton from 'material-ui/lib/raised-button';
+import Dialog from 'material-ui/lib/dialog';
+import FlatButton from 'material-ui/lib/flat-button';
+
+const muiTheme = getMuiTheme({
+	palette: {
+		accent1Color: deepOrange500,
+	},
+});
+
 const MainLayout = React.createClass({
+
+	getInitialState: function () {
+		return {
+			open: false
+		};
+	},
+
+	handleTouchTap: function () {
+		this.setState({
+			open: true
+		});
+	},
+
+	handleRequestClose: function () {
+		this.setState({
+			open: false
+		});
+	},
+
 	render: function () {
+		const standardActions = (
+			<FlatButton
+				label="Okey"
+				secondary={true}
+				onTouchTap={this.handleRequestClose}
+			/>
+		);
+
 		return (
-			<div className="app">
-				<header className="primary-header"></header>
-				<aside className="primary-aside">
-					<ul>
-						<li><Link to="/" activeClassName="active">Home</Link></li>
-						<li><Link to="/something-else" activeClassName="active">Something Else</Link></li>
-					</ul>
-				</aside>
-				<main>
-					{ this.props.children }
-				</main>
-			</div>
+			<MuiThemeProvider muiTheme={muiTheme}>
+				<div className="app">
+					<header className="primary-header"></header>
+					<Dialog
+						open={this.state.open}
+						title="Super Secret Password"
+						actions={standardActions}
+						onRequestClose={this.handleRequestClose}
+					>
+					1-2-3-4-5
+					</Dialog>
+					<aside className="primary-aside">
+						<ul>
+							<li><Link to="/" activeClassName="active">Home</Link></li>
+							<li><Link to="/something-else" activeClassName="active">Something Else</Link></li>
+						</ul>
+					</aside>
+					<RaisedButton
+						label="Super Secret Password"
+						primary={true}
+						onTouchTap={this.handleTouchTap}
+					/>
+					<main>
+						{ this.props.children }
+					</main>
+				</div>
+			</MuiThemeProvider>
 		);
 	}
 });
--- app/main.js
+++ app/main.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import Router from './router';
+
+// Needed for onTouchTap
+// Can go away when react 1.0 release
+// Check this repo:
+// https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
 ReactDOM.render(Router, document.getElementById('root'));
