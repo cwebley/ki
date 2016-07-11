defe2f78524cfa6d1e6605b95d34479bfeb2c612
Cameron <cameron.webley@gmail.com>
2016-07-11 18:23:53 -0500

    sort tournament_characters by streak then pick order

--- api/lib/queries/get-tournament-characters.js
+++ api/lib/queries/get-tournament-characters.js
@@ -6,16 +6,20 @@ export default function getTournamentCharactersQuery (db, tournamentUuid, userUu
 			c.name, c.season, c.slug, c.uuid,
 			tc.wins, tc.losses, tc.streak, tc.best_streak AS "bestStreak",
 			tc.value, tc.raw_value AS "rawValue", tc.fire_wins AS "fireWins",
-			uc.streak AS "globalStreak", uc.best_streak AS "globalBestStreak"
+			uc.streak AS "globalStreak", uc.best_streak AS "globalBestStreak",
+			d.pick
 		FROM characters AS c
 			JOIN tournament_characters AS tc
 				ON tc.character_uuid = c.uuid
 			JOIN user_characters AS uc
 				ON uc.user_uuid = tc.user_uuid
 				AND uc.character_uuid = c.uuid
+			LEFT JOIN drafts d
+				ON d.tournament_uuid = tc.tournament_uuid
+				AND d.character_uuid = c.uuid
 		WHERE tc.tournament_uuid = $1
 			AND tc.user_uuid = $2
-			ORDER BY tc.streak DESC
+			ORDER BY tc.streak DESC, d.pick ASC
 	`;
 	const params = [tournamentUuid, userUuid];
 
