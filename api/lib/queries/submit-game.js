4eddd288ced9de127c8c14bafae51de42c8160ee
Cameron <cameron.webley@gmail.com>
2016-07-22 01:15:54 -0500

    fix losing character streak bug on undogame

--- api/lib/queries/submit-game.js
+++ api/lib/queries/submit-game.js
@@ -246,8 +246,8 @@ function insertGame (db, tournamentUuid, game, cb) {
 				winning_character_previous_global_streak,
 				losing_player_previous_streak,
 				losing_player_previous_global_streak,
-				losing_character_previous_global_streak,
 				losing_character_previous_streak,
+				losing_character_previous_global_streak,
 				supreme
 			)
 		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)
