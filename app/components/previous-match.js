4b7426b5542e9efd62c247c4166d2eba2877d49d
Cameron <cameron.webley@gmail.com>
2016-10-21 23:00:42 -0500

    previous match styling things

--- app/components/previous-match.js
+++ app/components/previous-match.js
@@ -6,13 +6,29 @@ export default function previousMatch ({leftUser, rightUser, previousMatch}) {
 	let leftPreviousData;
 	let rightPreviousData;
 
+	let leftUserStyle = {};
+	let rightUserStyle = {};
+
 	if (previousMatch.result[0] === leftUser.uuid) {
 		leftPreviousData = previousMatch.ids[previousMatch.result[0]];
 		rightPreviousData = previousMatch.ids[previousMatch.result[1]];
+		rightUserStyle.opacity = .5;
+
+		if (previousMatch.supreme) {
+			leftUserStyle.border = '3px solid ' + green500;
+			leftUserStyle.borderRadius = '1em';
+		}
 	}
+
 	if (previousMatch.result[1] === leftUser.uuid) {
 		leftPreviousData = previousMatch.ids[previousMatch.result[1]];
 		rightPreviousData = previousMatch.ids[previousMatch.result[0]];
+		leftUserStyle.opacity = .5;
+
+		if (previousMatch.supreme) {
+			rightUserStyle.border = '3px solid ' + green500;
+			rightUserStyle.borderRadius = '1em';
+		}
 	}
 
 	const leftStreak = intToStreak(leftPreviousData.characterStreak);
@@ -20,11 +36,7 @@ export default function previousMatch ({leftUser, rightUser, previousMatch}) {
 
 	const commonStreakStyles = {
 		marginLeft: '1rem'
-	}
-	const valueStyles = {
-		marginRight: '1rem',
-		fontWeight: 600
-	}
+	};
 
 	let leftStreakStyle = Object.assign({}, commonStreakStyles);
 	if (leftPreviousData.characterStreak > 0) {
@@ -42,16 +54,21 @@ export default function previousMatch ({leftUser, rightUser, previousMatch}) {
 		rightStreakStyle.color = red500;
 	}
 
+	const valueStyles = {
+		marginRight: '1rem',
+		fontWeight: 600
+	};
+
 	return (
 		<div style={{paddingTop: '1em'}}>
 			<h5>Previous Match:</h5>
-			<div>
+			<div style={leftUserStyle}>
 				<span style={valueStyles}>{leftPreviousData.value}</span>
 				<span>{leftUser.characters.ids[leftPreviousData.characterUuid].name}</span>
 				<span style={leftStreakStyle}>{leftStreak}</span>
 			</div>
 			vs
-			<div>
+			<div style={rightUserStyle}>
 				<span style={valueStyles}>{rightPreviousData.value}</span>
 				<span>{rightUser.characters.ids[rightPreviousData.characterUuid].name}</span>
 				<span style={rightStreakStyle}>{rightStreak}</span>
@@ -64,4 +81,4 @@ previousMatch.propTypes = {
 	leftUser: PropTypes.object.isRequired,
 	rightUser: PropTypes.object.isRequired,
 	previousMatch: PropTypes.object.isRequired
-}
+};
